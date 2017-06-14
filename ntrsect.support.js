4e095c77415c5efd962259e369824d95f3de3f62
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-14 10:13:45 +0800

    Updated client test.

--- ntrsect.support.js
+++ ntrsect.support.js
@@ -35,6 +35,9 @@
               			"module": "ntrsect",
               			"author": "Richeve S. Bebedor",
               			"eMail": "richeve.bebedor@gmail.com",
+              			"contributors": [
+              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+              			],
               			"repository": "https://github.com/volkovasystems/ntrsect.git",
               			"test": "ntrsect-test.js",
               			"global": true
