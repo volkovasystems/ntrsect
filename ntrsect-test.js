c53886db4835d7d00e7223cd8b994293884974be
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-28 14:30:40 +0800

    Updated test.

--- ntrsect-test.js
+++ ntrsect-test.js
@@ -2,6 +2,7 @@
 const assert = require( "assert" );
 const ntrsect = require( "./ntrsect.js" );
 
-assert.deepEqual( ntrsect( [ 1, 2, 3, 4, 5 ], [ 6, 7, 1, 2, 3 ] ), [ 1, 2, 3 ], "should be deeply equal" );
+assert.deepEqual( ntrsect( [ 1, 2, 3, 4, 5 ], [ 6, 7, 1, 2, 3 ] ),
+	[ 1, 2, 3 ], "should be equal to [ 1, 2, 3 ]" );
 
 console.log( "ok" );
